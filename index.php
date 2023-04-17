31ac22e7100105b9709275946909a29e647f9997
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-04-17 15:19:37 +0200

    creazione form

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bad Words</title>
+</head>
+<body>
+    
+    <main>
+        <form method="GET" action="censura.php">
+
+        <label for="paragrafo">Inserire Paragrafo:</label>
+        <input type="text" id="paragrafo" name="paragrafo" placeholder="...">
+
+        <button type="submit">Invia</button>
+        </form>
+    </main>
+
+</body>
+</html>
